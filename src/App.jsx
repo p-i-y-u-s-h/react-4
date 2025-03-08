82b529e1872b7561827bc81ed7b9307028fc250c
Piyush Mahajan <mahajanpiyush005@gmail.com>
2025-03-09 01:49:45 +0530

    commit-3

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,34 @@ function App() {
     <div style={{background: "#c8dceb", height:"100vh"}}>
       <div style={{display:"flex",justifyContent:"center"}}>
         <div>
-          <div>
+        <div>
             <br />
-            <Postcomponent/>
+            <Postcomponent
+              Name="Piyush"
+              followerCount={20}
+              time="2m ago"
+              image="https://appx-wsb-gcp.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg"
+              description="What am I doing at 3 AM?"
+            />
           </div>
           <div>
             <br />
-            <Postcomponent/>
+            <Postcomponent
+              Name="John Doe"
+              followerCount={50}
+              time="10m ago"
+              image="https://appx-wsb-gcp.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg"
+              description="Coding late at night!"
+            />
           </div>
           <div>
             <br />
-            <Postcomponent/>
+            <Postcomponent
+              Name="Jane Smith"
+              subtitle={"promoted"}
+              image="https://appx-wsb-gcp.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg"
+              description="Working on a new project!"
+            />
           </div>
         </div>
       </div>
@@ -23,26 +40,31 @@ function App() {
   )
 }
 
-const style = {width : 200 , backgroundColor:"white",borderRadius:10,borderColor:"gray",borderWidth:1,padding:20}
+const style = {width : 300, backgroundColor:"white",borderRadius:10,borderColor:"gray",borderWidth:1,padding:20}
 
-function Postcomponent(){
+function Postcomponent({ Name, followerCount, time, image, description,subtitle }){
   return <div style={style}>
    <div style={{display:"flex"}}>
-    <img src="https://appx-wsb-gcp.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg" style={{
+    <img src={image}style={{
       width:20,
       height:20,
       borderRadius:20
     }} />
     <div style={{fontSize:10,marginLeft:10}}>
       <b>
-        100xdevs
+        {Name}
       </b>
-      <div>865498</div>
-      <div>12m</div>
+      <div>{subtitle}</div>
+      <div>{followerCount} follwers</div>
+      {time !== undefined &&
+        <div style={{display:"flex"}}>
+        <div>{time} </div>
+        <img src={"https://appx-wsb-gcp.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg"} style={{width:10,height:10}}/>
+      </div>}
     </div>
   </div>
   <div>
-    what is your goal
+    {description}
   </div>
 </div>
 }
