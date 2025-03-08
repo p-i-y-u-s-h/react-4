e5b5fe9add909140c23f9d0002a95e03ff9abe7e
Piyush Mahajan <mahajanpiyush005@gmail.com>
2025-03-09 00:25:10 +0530

    commit-1

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+
+function App() {
+
+  return (
+    <div style={{background: "#c8dceb", height:"100vh"}}>
+      <Postcomponent/>
+    </div>
+  )
+}
+
+const style = {width : 200 , backgroundColor:"white",borderRadius:10,borderColor:"gray",borderWidth:1}
+
+function Postcomponent(){
+  return <div style={style}>
+   <div style={{display:"flex"}}>
+    <img src="https://appx-wsb-gcp.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg" style={{
+      width:20,
+      height:20,
+      borderRadius:20
+    }} />
+    <div style={{fontSize:10,marginLeft:10}}>
+      <b>
+        100xdevs
+      </b>
+      <div>865498</div>
+      <div>12m</div>
+    </div>
+  </div>
+  <div>
+    what is your goal
+  </div>
+</div>
+}
+
+export default App
