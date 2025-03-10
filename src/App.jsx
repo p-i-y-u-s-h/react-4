95c645d087ea295037f8e16400cda797a013ad53
Piyush Mahajan <mahajanpiyush005@gmail.com>
2025-03-10 21:23:32 +0530

    commit-4

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,62 @@
+import { useState } from "react"
 
 function App() {
 
   return (
     <div style={{background: "#c8dceb", height:"100vh"}}>
-      <div style={{display:"flex",justifyContent:"center"}}>
+      <ToggleMessage/>
+    </div>
+  )
+}
+
+
+const ToggleMessage = () => {
+  const [isVisible , setIsVisible] = useState(false);
+
+  return (
+    <div>
+      <button onClick={() => setIsVisible(!isVisible)}>
+        toggle Message
+      </button>
+      {isVisible && <P>this Message is conditionally rendered!</P>}
+    </div>
+  );
+}
+
+
+
+export default App
+
+// const style = {width : 300, backgroundColor:"white",borderRadius:10,borderColor:"gray",borderWidth:1,padding:20}
+
+// function Postcomponent({ Name, followerCount, time, image, description,subtitle }){
+//   return <div style={style}>
+//    <div style={{display:"flex"}}>
+//     <img src={image}style={{
+//       width:20,
+//       height:20,
+//       borderRadius:20
+//     }} />
+//     <div style={{fontSize:10,marginLeft:10}}>
+//       <b>
+//         {Name}
+//       </b>
+//       <div>{subtitle}</div>
+//       <div>{followerCount} follwers</div>
+//       {time !== undefined &&
+//         <div style={{display:"flex"}}>
+//         <div>{time} </div>
+//         <img src={"https://appx-wsb-gcp.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg"} style={{width:10,height:10}}/>
+//       </div>}
+//     </div>
+//   </div>
+//   <div>
+//     {description}
+//   </div>
+// </div>
+// }
+
+{/* <div style={{display:"flex",justifyContent:"center"}}>
         <div>
         <div>
             <br />
@@ -36,38 +89,4 @@ function App() {
             />
           </div>
         </div>
-      </div>
-    </div>
-  )
-}
-
-const style = {width : 300, backgroundColor:"white",borderRadius:10,borderColor:"gray",borderWidth:1,padding:20}
-
-function Postcomponent({ Name, followerCount, time, image, description,subtitle }){
-  return <div style={style}>
-   <div style={{display:"flex"}}>
-    <img src={image}style={{
-      width:20,
-      height:20,
-      borderRadius:20
-    }} />
-    <div style={{fontSize:10,marginLeft:10}}>
-      <b>
-        {Name}
-      </b>
-      <div>{subtitle}</div>
-      <div>{followerCount} follwers</div>
-      {time !== undefined &&
-        <div style={{display:"flex"}}>
-        <div>{time} </div>
-        <img src={"https://appx-wsb-gcp.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg"} style={{width:10,height:10}}/>
-      </div>}
-    </div>
-  </div>
-  <div>
-    {description}
-  </div>
-</div>
-}
-
-export default App
+      </div> */}
