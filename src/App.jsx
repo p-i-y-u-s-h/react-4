e15ad710355e97d9ef7fc74a4fd5e1eb5af59a20
Piyush Mahajan <mahajanpiyush005@gmail.com>
2025-03-10 23:12:30 +0530

    commit-6

--- src/App.jsx
+++ src/App.jsx
@@ -1,97 +1,26 @@
-import { useState } from "react"
+import { Postcomponent } from "./post";
 
-function App() {
+function App(){
+  function addPost(){
+
+  }
 
   return (
-    <div style={{background: "#c8dceb", height:"100vh"}}>
-      <ToggleMessage/>
+    <div style={{background:"black",height:"100vh"}}>
+      <button onClick={addPost}>Add post</button>
+      <div style={{display:"flex",justifyContent:"center"}}>
+        <div>
+          <Postcomponent
+            name={"piyush"}
+            subtitle={"111111 followers"}
+            time={"2m ago"}
+            image={"https://appx-wsb-gcp.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg"}
+            description={"what am i doing"}
+          />
+        </div>
+      </div>
     </div>
   )
 }
 
-
-const ToggleMessage = () => {
-
-  let [isVisible,setIsInterval]=useState(true);
-
-  function toggle(){
-    setIsInterval(!isVisible);
-  }
-
-  return (
-    <div>
-      <button onClick={toggle}>
-        toggle Message
-      </button>
-      {isVisible && <p>this Message is conditionally rendered!</p>}
-    </div>
-  );
-};
-
-
-
 export default App
-
-// const style = {width : 300, backgroundColor:"white",borderRadius:10,borderColor:"gray",borderWidth:1,padding:20}
-
-// function Postcomponent({ Name, followerCount, time, image, description,subtitle }){
-//   return <div style={style}>
-//    <div style={{display:"flex"}}>
-//     <img src={image}style={{
-//       width:20,
-//       height:20,
-//       borderRadius:20
-//     }} />
-//     <div style={{fontSize:10,marginLeft:10}}>
-//       <b>
-//         {Name}
-//       </b>
-//       <div>{subtitle}</div>
-//       <div>{followerCount} follwers</div>
-//       {time !== undefined &&
-//         <div style={{display:"flex"}}>
-//         <div>{time} </div>
-//         <img src={"https://appx-wsb-gcp.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg"} style={{width:10,height:10}}/>
-//       </div>}
-//     </div>
-//   </div>
-//   <div>
-//     {description}
-//   </div>
-// </div>
-// }
-
-{/* <div style={{display:"flex",justifyContent:"center"}}>
-        <div>
-        <div>
-            <br />
-            <Postcomponent
-              Name="Piyush"
-              followerCount={20}
-              time="2m ago"
-              image="https://appx-wsb-gcp.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg"
-              description="What am I doing at 3 AM?"
-            />
-          </div>
-          <div>
-            <br />
-            <Postcomponent
-              Name="John Doe"
-              followerCount={50}
-              time="10m ago"
-              image="https://appx-wsb-gcp.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg"
-              description="Coding late at night!"
-            />
-          </div>
-          
-          <div>
-            <br />
-            <Postcomponent
-              Name="Jane Smith"
-              subtitle={"promoted"}
-              image="https://appx-wsb-gcp.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg"
-              description="Working on a new project!"
-            />
-          </div>
-        </div>
-      </div> */}
---
+++ src/post.jsx
@@ -0,0 +1,31 @@
+
+const style = {width:200, backgroundColor:"white", borderRadius:10, borderColor:"gray", borderWidth:1,padding:20}
+
+export function Postcomponent({name,subtitle,time,image,description}){
+    return <div style={style}>
+        <div style={{display:"flex"}}>
+            <img src={image} style={{
+                width:30,
+                height:30,
+                borderRadius:20
+            }}/>
+            <div style={{fontSize:10, marginLeft:10}}>
+                <b>
+                    {name}
+                </b>
+                <div>
+                    {subtitle}
+                </div>
+                {(time !== undefined) ? <div>
+                    <div>
+                        {time}
+                    </div>
+                    <img src={"https://www.vecteezy.com/vector-art/4397047-clock-icon-clock-time-symbol-flat-style-design-web-site-icon-logo-app-ui-illustration-vector-eps10"} style={{width:12,height:12}}/>
+                </div>:null}
+            </div>
+        </div>
+        <div style={{fontSize:12}}>
+            {description}
+        </div>
+    </div>
+}
